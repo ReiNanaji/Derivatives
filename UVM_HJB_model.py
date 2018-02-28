e3f58e3a4e92f903a09aea09f6d0952360c6e7c5
ReiNanaji <36332594+ReiNanaji@users.noreply.github.com>
2018-02-28 13:57:32 -0500

    Update UVM_HJB_model.py

--- UVM_HJB_model.py
+++ UVM_HJB_model.py
@@ -51,10 +51,27 @@ def UVMHJB_pde_pricer(smin, smax, beta, zero_curve, payoff, grid, upper=True):
     
     C = np.diag(np.ones(grid['X']['step'] - 1), k=-1) + np.diag(-2*np.ones(grid['X']['step']), k=0) + \
         np.diag(np.ones(grid['X']['step'] - 1), k=1)
+    
+    '''    
+    Approximation with the first order derivative:
+    ----------------------------------------------
     C[0, 0] = -1
     C[0, 1] = 1
     C[-1, -1] = 1
     C[-1, -2] = -1
+    '''
+    
+    '''
+    Approximation with the next second order derivative:
+    ----------------------------------------------------
+    '''
+    C[0,0] = 1
+    C[0,1] = -2
+    C[0,2] = 1
+    
+    C[-1,-1] = 1
+    C[-1,-2] = -2
+    C[-1,-3] = 1
     
     dt = grid['T']['incr']
     dx = grid['X']['incr']
@@ -68,7 +85,7 @@ def UVMHJB_pde_pricer(smin, smax, beta, zero_curve, payoff, grid, upper=True):
     
     # Initialize Pricing Matrix
     U = np.zeros((grid['X']['step'], grid['Y']['step'], grid['T']['step']))
-    U[:,:,-1] = payoff(X[:,:,-1], Y[:,:,-1])    
+    U[:,:,-1] = payoff(X[:,:,-1], Y[:,:,-1], grid['T']['step'], 0.125, beta, zero_curve, 0.05)    
     
     # Solve the PDE
     for t in range(N - 2, -1, -1):
@@ -81,8 +98,29 @@ def UVMHJB_pde_pricer(smin, smax, beta, zero_curve, payoff, grid, upper=True):
         0.5 * V * V * M - (F[:,:,t+1] + X[:,:,t+1]) * U[:,:,t+1] * dt
     return {'grid':grid, 'zero':F, 'price':U}
 
-def bond(X, Y):
+def bond(X, Y, T=None, delta=None, beta=None, zero_curve=None, K=None):
     return np.ones(X.shape)
+    
+def forward(X, Y, T, delta, beta, zero_curve, K=None):
+    time = np.linspace(T, T + delta, 100)
+    dt = time[1] - time[0]
+    b = np.exp( dt * np.sum( zero_curve( time[:-1] ) ) )
+    k = np.exp(-beta*delta)
+    g = ( 1 - k ) / beta    
+    Libor = (b * np.exp( - ( 0.5 * Y * (1 - k * k) - X * g ) ) - 1 ) / delta 
+    return Libor
+    
+def caplet(X, Y, T, delta, beta, zero_curve, K):
+    Libor = forward( X, Y, T, delta, beta, zero_curve )
+    P = Libor - K
+    P[P < 0] = 0
+    return P
+    
+def floorlet(X, Y, T, delta, beta, zero_curve, K):
+    Libor = forward( X, Y, T, delta, beta, zero_curve )
+    P = K - Libor
+    P[P < 0]
+    return P 
 
 def bond_price(beta, grid, zero_curve):
     dt = grid['T']['incr']
@@ -105,15 +143,17 @@ def bond_price(beta, grid, zero_curve):
 
 
 Tstart, Tend, Tstep = 0, 1, 101
-Xstart, Xend, Xstep = -0.25, 0.25, 11
-Ystart, Yend, Ystep = 0, 0.005, 11
+Xstart, Xend, Xstep = -0.25, 0.25, 21
+Ystart, Yend, Ystep = 0, 0.005, 21
 
-smin, smax = 0.05, 0.05
+smin, smax = 0.05, 0.2
 beta = 0.05
 
 grid = get_regular_grid(Tstart, Tend, Tstep, Xstart, Xend, Xstep, Ystart, Yend, Ystep)
     
-res = UVMHJB_pde_pricer(smin, smax, beta, zero_curve, bond, grid, upper=True)
+upper_res = UVMHJB_pde_pricer(smin, smax, beta, zero_curve, bond, grid, upper=True)
+lower_res = UVMHJB_pde_pricer(smin, smax, beta, zero_curve, bond, grid, upper=False)
+
 
 close = bond_price(beta, grid, zero_curve)
 
@@ -121,23 +161,36 @@ close = bond_price(beta, grid, zero_curve)
 
 '''
 Comparing the bond price from the zero curve using close form formula or the PDE solver
-'''
 
-i = 1
-j = 1
 
-#plt.figure()
-#plt.plot(grid['T']['matrix'][0,0,:], res['price'][i,j,:] - close[i,j,:])
-#plt.grid()
-#plt.legend(loc=0)
-#plt.show()
-
+i = 3
+j = 3
 
 plt.figure()
-plt.plot(grid['T']['matrix'][0,0,:], res['price'][i,j,:], label='PDE')
+plt.plot(grid['T']['matrix'][0,0,:], upper_res['price'][i,j,:], label='PDE')
 plt.plot(grid['T']['matrix'][0,0,:], close[i,j,:], label='Exact')
 plt.grid()
 plt.legend(loc=0)
 plt.show()
+'''
+'''
+3D PLOT
+'''
+
+pos = 0
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.set_xlabel('Gamma (X)')
+ax.set_ylabel('Phi (Y)')
+ax.set_zlabel('Difference')
+#ax.plot_surface(grid['X']['matrix'][:,:,pos], grid['Y']['matrix'][:,:,pos], close[:,:,pos] - upper_res['price'][:,:,pos], linewidth=2)
+ax.plot_surface(grid['X']['matrix'][:,:,pos], grid['Y']['matrix'][:,:,pos], upper_res['price'][:,:,pos], linewidth=2)
+ax.plot_surface(grid['X']['matrix'][:,:,pos], grid['Y']['matrix'][:,:,pos], lower_res['price'][:,:,pos], linewidth=2)
+#ax.plot_surface(grid['X']['matrix'][:,:,pos], grid['Y']['matrix'][:,:,pos], upper_res['price'][:,:,pos] - lower_res['price'][:,:,pos], linewidth=2)
+plt.show()
+
+#plt.savefig('M:\Vathana\Notes\sanity_check2_t0.pdf', format='pdf')
+
 
 
